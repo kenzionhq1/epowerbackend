8bfcec4ee4dac9764e578bde63cac25bf5d5f67b
OGUNFOWORA TWINS <OGUNFOWORATWINS@OGUNFOWORA-TWINS-macBOOK-AIR.local>
2025-06-07 15:38:40 +0100

    “done”

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS FIX
 app.use(cors({
-  origin: "https://e-power-beryl.vercel.app/",
+  origin: "https://e-power-beryl.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
